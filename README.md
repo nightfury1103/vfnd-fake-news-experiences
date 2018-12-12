ef794b1d4f6e0fb6d3e1309926909a7ea443fa17
Unknown <thanh.hoquangcse96@gmail.com>
2018-12-12 16:54:00 +0700

    Extending the dictionary

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # VFND Experiences
 
 Một số thí nghiệm dựa trên dataset [VFND](https://github.com/thanhhocse96/vfnd-vietnamese-fake-news-datasets) sẽ được thực hiện trong repo này
+
+## Mô tả nội dung các file
+
+_corpus_extending.ipynb_: dùng để mở rộng bộ từ điển để dùng cho phương pháp Longest Matching
+
+_news-preprocessing-exp\[#no\].ipynb_: thử nghiệm các phương thức tiền xử lý dữ liệu
--- utils.py
+++ utils.py
@@ -4,8 +4,22 @@ import ast, os
 load_n_gram(corpus_path): load data from corpus_path
 """
 def load_n_gram(corpus_path):
-    with open(corpus_path, encoding = 'utf-8') as file:
-        words = file.read()
-        words = ast.literal_eval(words)
-        return words
+  with open(corpus_path, encoding = 'utf-8') as file:
+    words = file.read()
+    words = ast.literal_eval(words)
+    return words
 
+"""
+load_stopwords: load stopwords from 'stopwords.txt' file
+Output: Set of stopwords
+Stopwords.txt description: 1 word 1 line + 2 python style commments
+"""
+def load_stopwords(stopwords_path):
+  words = set()
+  with open(stopwords_path, encoding='utf-8') as file:
+    for line in file:
+      li=line.strip()
+      if not li.startswith("#"):
+        words.add(line.rstrip())
+              
+  return words
